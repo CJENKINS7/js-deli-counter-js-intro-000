4c3e2d89806a20684e3dd42280dbbdf8a58f7c5e
congenial-principle-5422 <christinajenkins10@gmail.com>
2020-03-20 02:14:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function currentLine(katzDeliLine) {
     return "The line is currently empty."
   }
 
-  const numbersAndNames = []
+  var numberwithnames = []
 
   for (let i = 0, l = line.length; i < l; i++) {
     numbersAndNames.push(`${i + 1}. ${line[i]}`)
